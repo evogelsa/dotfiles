1dc3e293d9c9576442023114b02c6208886c6ffc
Ethan Vogelsang <ethanvogelsang@gmail.com>
2020-08-11 01:10:37 -0400

    update screenshot clip to copy to clip & fix dimming

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -1,2 +1,2 @@
 mkdir -p $HOME/Pictures/screenshots
-scrot -a $(slop -f '%x,%y,%w,%h') $HOME/Pictures/screenshots/screenshot_%Y-%m-%d_%H%M%S.png
+scrot -a $(slop -f '%x,%y,%w,%h') $HOME/Pictures/screenshots/screenshot_%Y-%m-%d_%H%M%S.png -e 'xclip -sel clip -t image/png < $f'
